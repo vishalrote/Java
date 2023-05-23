d6037bd161dd28a4d5fddab469bfa4f17cfdd19c
Vishal Rote <96374713+vishalrote@users.noreply.github.com>
2023-05-23 10:23:29 +0530

    Add files via upload

---
+++ Java Basics/variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String[] args) {
+
+        int x = 13223; // initialization
+        System.out.println("My Number is :" + x);
+
+        long y = 1253231342;
+        System.out.println("Second Number is: " + y);
+
+        //float a = 3.14f; //or double
+        double a = 3.14;//no f 
+        boolean z = true;
+        char symbol = '@';
+        String name = "Vishal";
+
+        System.out.print(name + symbol + a + z );
+    }
+}
